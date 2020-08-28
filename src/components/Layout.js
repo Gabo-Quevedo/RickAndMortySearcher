8d9552140f073f1376d569d948207e619883dba0
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-08-28 16:02:04 -0400

    inicio de proyecto rickAndMorty

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Layout (props) {
+    // const children = props.childen
+    return (
+    <React.Fragment>
+        <p>ejemplo</p>
+    </React.Fragment>
+    ) 
+}
+
+export default Layout
