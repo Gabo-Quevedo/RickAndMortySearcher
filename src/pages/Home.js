f77bb550545fb2923887354ec0a61ffe63ca11bf
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-08-28 22:00:24 -0400

    main mobile terminado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import './styles/home.css'
+
+import rickMobile from '../images/rick.png'
+import portal from '../images/portal.png'
+import WALA from '../images/WALA.png'
+
 
 export default class Home extends Component {
   render() {
     return (
-      <div>asad</div>
+      <main className="main">
+        <div className="main__content">
+        <picture>
+          {/* <source media="(min-width:size*pordefinir)" srcset={expresion}/> */}
+          <img src={rickMobile} alt="Logo Rick and Morty API Searcher"/>
+        </picture>
+        <picture>
+          {/* <source media="(min-width:size*pordefinir)" srcset={expresion}/> */}
+          <img src={WALA} alt="Logo Rick and Morty API Searcher"/>
+        </picture>
+        <picture>
+          {/* <source media="(min-width:size*pordefinir)" srcset={expresion}/> */}
+          <img src={portal} alt="Logo Rick and Morty API Searcher"/>
+        </picture>
+        </div>
+      </main>
     );
   }
 }
