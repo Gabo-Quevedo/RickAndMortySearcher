e6a58a01eae3c8d2f929bc946303d5e4a2ea2c42
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-08-29 00:45:17 -0400

    consumo de api en CharBadge exitoso, nombres y foto de personajes

---
+++ src/components/CharBadge.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// import examp from '../images/1.jpeg'
+
+class CharBadge extends React.Component{
+    
+    render(){
+        return (
+                <div className="fillList__content">
+                <div className="fillList__content-card">
+                    <img src={this.props.character.image} alt="Foto de personaje"/>
+                </div>
+                <div className="fillList_content_nameContent">
+                    <label className="fillList_content_nameContent-name">{this.props.character.name}</label>
+                </div>    
+            </div>
+            
+        )
+    }
+}
+
+export default CharBadge
--- src/pages/Searched.js
+++ src/pages/Searched.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import CharBadge from '../components/charBadge'
 
-import examp from '../images/1.jpeg'
+
 
 import './styles/searched.css'
 
 
 
 export default class Searched extends Component {
+    state = {
+        data: {
+            results: [],
+        }
+    }
+componentDidMount(){
+    this.fetchCharacters()
+}
+fetchCharacters = async () => {
+    const response = await fetch('https://rickandmortyapi.com/api/character/')
+    const data = await response.json()
+
+    this.setState({
+        data: data
+    })
+}
   render() {
     return (
         <React.Fragment>
             <main>
-                <section className="fillList">
-                    <div className="fillList__content">
-                        <div className="fillList__content-card">
-                            <img src={examp} alt="Foto de personaje"/>
-                        </div>
-                        <div className="fillList_content_nameContent">
-                            <label className="fillList_content_nameContent-name">Rick Sanchez</label>
-                        </div>
-                    </div>
+            {this.state.data.results.map(character => (
+                <section className="fillList" key={character.id}>
+                    <CharBadge character={character} />
                 </section>
+                ))}
             </main>
       </React.Fragment>
     );
