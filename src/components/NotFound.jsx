c4b27b4495305a443deb1dd6e614f5c99a28aa59
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-09-03 02:54:48 -0400

    page not found incorporada con exito

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import PNF from '../images/404.png';
+
+import './styles/notFound.css';
 
 function NotFound() {
   return (
-    <h2>NotFound</h2>
+    <>
+      <div className="notFoundContainer">
+        <div className="notFoundContainer__messaje">
+          <h1>Do NOT play clever impostor</h1>
+        </div>
+        <picture className="notFoundContainer__picture">
+          <img src={PNF} alt="404 Error" />
+        </picture>
+        <div className="notFoundContainer__picture-footer">Page Not Found</div>
+      </div>
+    </>
   );
 }
 
